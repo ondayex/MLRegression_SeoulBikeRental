0dac69e0d57ca9480faf91ffb97bbcad1eb94bc2
Andy Chen <82803774+ondayex@users.noreply.github.com>
2024-08-21 17:47:48 +0800

    added bar plot and histograms

--- data_exploration.py
+++ data_exploration.py
@@ -1,8 +1,10 @@
 # %%
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 # %%
-# load dataset and check for missing values and see the data types
+# load dataset，check for missing values and see the data types
 dataset = pd.read_csv('dataset/SeoulBikeData.csv')
 dataset.info()
 print(dataset.head(30))
@@ -13,6 +15,33 @@ print(dataset['Seasons'].unique())
 print(dataset['Holiday'].unique())
 print(dataset['Functioning Day'].unique())
 
+# %%
+#Statistical summary of numerical features (central tendencies, spread, and shape of the data)
+print(dataset.iloc[:, [2,3,4,5,6,7,8,9,10]].describe())
+
+dataset[['Hour','Temperature','Humidity(%)','Wind speed (m/s)','Visibility (10m)','Dew point temperature','Solar Radiation (MJ/m2)','Rainfall(mm)','Snowfall (cm)']].hist(bins=20, edgecolor='black', figsize=(12, 6))
+plt.show()
+# %%
+#Statistical summary of categorical features (distribution)
+print(dataset.iloc[:, [11,12,13]].describe())
+# Plot bar plot
+selected_columns = dataset.iloc[:, [11, 12, 13]]
+column_names = selected_columns.columns
+
+# Create a plot for each column
+for column in column_names:
+    # Count the occurrences of each category
+    category_counts = selected_columns[column].value_counts()
+    
+    # Plot the bar plot
+    plt.figure()  # Create a new figure for each plot
+    category_counts.plot(kind='bar', edgecolor='black', color='skyblue')
+    
+    plt.title(f'Distribution of {column}')
+    plt.xlabel(column)
+    plt.ylabel('Count')
+    plt.xticks(rotation=45, ha='right')  # Rotate x-axis labels for better readability
+    plt.show()
 # %%
 #process categorical features
 Seasons_dummies = pd.get_dummies(dataset['Seasons'], drop_first = True, dtype = int)
