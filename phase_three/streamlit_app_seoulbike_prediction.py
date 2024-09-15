c9fef0d523359537ebaeb448381295a933e4b2e5
Andy Chen <82803774+ondayex@users.noreply.github.com>
2024-09-15 15:38:23 +0800

    added dot to file path

--- phase_three/streamlit_app_seoulbike_prediction.py
+++ phase_three/streamlit_app_seoulbike_prediction.py
@@ -7,7 +7,7 @@ import os
 st.title('Bike Rental Prediction App')
 
 # Check if the model file exists
-model_path = '/phase_three/bike_rental_model_xgboost.pkl'
+model_path = './phase_three/bike_rental_model_xgboost.pkl'
 model_exists = os.path.exists(model_path)
 
 if not model_exists:
